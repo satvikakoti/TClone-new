f2ee69e05de8a7fc53d07159af784cbbfd5da91d
projectTC <85979410+projectTC@users.noreply.github.com>
2021-07-12 00:31:12 +0530

    Delete TeamsCloneProject/chat directory

--- TeamsCloneProject/chat/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import main_view, video_view, join_view
-
-urlpatterns = [
-    path('', main_view, name='main_view'),
-    path('join/', join_view, name='join_view'),
-    path('video/', video_view, name='video_view'),
-]
